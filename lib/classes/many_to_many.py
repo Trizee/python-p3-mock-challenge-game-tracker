0d6c8dca8517e6146ebd86ee4c98abdbbf3d22d3
Tri <trihoang09@gmail.com>
2023-09-18 22:37:51 -0600

    completed lab

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,93 @@
 class Game:
+
+    all = []
+
     def __init__(self, title):
-        self.title = title
+        self._title = title
+        Game.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self,title):
+        if isinstance(title,str) and not hasattr(self, "title") and title:
+            self._title = title
 
     def results(self):
-        pass
+        return [result for result in Result.all if result.game == self]
 
     def players(self):
-        pass
+        return list({result.player for result in self.results()})
 
     def average_score(self, player):
-        pass
-
+        scores = [result.score for result in player.results() if result.game is self]
+        return sum(scores)/len(scores)
+    
 class Player:
+
+    all = []
+
     def __init__(self, username):
         self.username = username
+        Player.all.append(self)
+
+    @property
+    def username(self):
+        return self._username
+    
+    @username.setter
+    def username(self,username):
+        if isinstance(username,str) and 1 < len(username) < 17:
+            self._username = username
 
     def results(self):
-        pass
+        return [result for result in Result.all if result.player == self]
 
     def games_played(self):
-        pass
+        return list({result.game for result in self.results()})
 
     def played_game(self, game):
-        pass
+       return game in self.games_played()
 
     def num_times_played(self, game):
-        pass
+        games_played = [result.game for result in self.results()]
+        return games_played.count(game)
 
 class Result:
+
+    all = []
+
     def __init__(self, player, game, score):
         self.player = player
         self.game = game
         self.score = score
+        Result.all.append(self)
+
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def score(self, score):
+        if isinstance(score, int) and not hasattr(self, "score") and 1 <= score <= 5000:
+            self._score = score
+
+    @property
+    def player(self):
+        return self._player
+
+    @player.setter
+    def player(self, player):
+        if isinstance(player, Player):
+            self._player = player
+
+    @property
+    def score(self):
+        return self._score
+    
+    @score.setter
+    def score(self,score):
+        if isinstance(score,int) and not hasattr(self,'score') and 0 < score <5001:
+            self._score = score
